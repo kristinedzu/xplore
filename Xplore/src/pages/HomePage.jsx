8f169b4fbdaf4a7ea6da5008c2735172b6b97f4b
Maddy <polakowska.mag@gmail.com>
2022-03-17 16:48:29 +0100

    Loader on loading Home Page

--- Xplore/src/pages/HomePage.jsx
+++ Xplore/src/pages/HomePage.jsx
@@ -48,7 +48,7 @@ export default function HomePage(){
     }else if(filteredCountriesArray.length == 0){
       setCountries(countriesArray);
     }
-    
+    dismissLoader();
   }
 
   async function getCities() {
