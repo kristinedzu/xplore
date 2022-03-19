34258c5b5087a7c914eab954b575a61b153f0558
Kristine Dzumakajeva <kristinedzu@gmail.com>
2022-03-20 00:13:31 +0100

    changing fetch on homepage to use snapshot instead

--- Xplore/src/pages/HomePage.jsx
+++ Xplore/src/pages/HomePage.jsx
@@ -6,8 +6,8 @@ import { useEffect, useState } from "react";
 import CountryItem from "../components/CountryItem";
 import { getAuth} from "firebase/auth";
 import { useHistory } from "react-router-dom";
-import { get } from "@firebase/database";
-import { getUserRef } from "../firebase-config";
+import { get, onValue } from "@firebase/database";
+import { getUserRef, countriesRef, postsRef, citiesRef } from "../firebase-config";
 
 
 
@@ -19,39 +19,95 @@ export default function HomePage(){
 
   
   const [countries, setCountries] = useState([]);
+  const [filtered, setFiltered] = useState([]);
   const [cities, setCities] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [firstName, setFirstName] = useState();
   const [profileImg, setProfileImg] = useState();
   const history = useHistory();
 
-  async function getPosts() {
-    const postsRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/posts.json`);
-    const postsData = await postsRes.json();
-    const posts = Object.keys(postsData).map(key => ({ id: key, ...postsData[key]})); // from object to array
-    return posts;
-  }
+  // // countries with posts
+  // const result = countries.map((country) => ({
+  //   data: country,
+  //   match: posts.some((post) => post.countryId === country.id)
+  // })).filter(res => res.match == true);
+
 
   async function getCountries() {
-    showLoader();
-    const postsArray = await getPosts();
+    // const postsArray = await getPosts();
+    // const postRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/posts.json`);
+    // const postData = await postRes.json();
+    // const allPosts = Object.keys(postData).map(key => ({ id: key, ...postData[key], })); // from object to array
 
-    const countriesRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/countries.json`);
-    const countriesData = await countriesRes.json();
-    const allCountries = Object.keys(countriesData).map(key => ({ id: key, ...countriesData[key], posts: postsArray.find(post => post.countryId == key)})); // from object to array
-    const countriesArray = allCountries.filter(country => country.posts != undefined);
-    dismissLoader();
-    setCountries(countriesArray);
-    
+    // const countriesRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/countries.json`);
+    // const countriesData = await countriesRes.json();
+    // const allCountries = Object.keys(countriesData).map(key => ({ id: key, ...countriesData[key]})); // from object to array
+
+    // // check if countries have any posts
+    // const result = allCountries.map((country) => ({
+    //   data: country,
+    //   match: allPosts.some((post) => post.countryId === country.id)
+    //   })).filter(res => res.match == true);
+    //   const countriesWithPosts = result.map(res => res.data);
+    //   console.log(countriesWithPosts);
+  
+    //   if(result.length != 0) {
+    //     setCountries(countriesWithPosts);
+    //   }
+
+      onValue(countriesRef, async snapshot => {
+          const countriesArray = [];
+          snapshot.forEach(postSnapshot => {
+            const id = postSnapshot.key;
+            const data = postSnapshot.val();
+  
+            const country = {
+                id,
+                ...data
+            };
+
+            countriesArray.push(country);
+          });
+          setCountries(countriesArray);
+      });
   }
 
   async function getCities() {
-    const postsArray = await getPosts();
+    // const postRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/posts.json`);
+    // const postData = await postRes.json();
+    // const allPosts = Object.keys(postData).map(key => ({ id: key, ...postData[key], })); // from object to array
 
-    const citiesRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/cities.json`);
-    const citiesData = await citiesRes.json();
-    const allCities = Object.keys(citiesData).map(key => ({ id: key, ...citiesData[key], posts: postsArray.find(post => post.cityId == key)})); // from object to array
-    const citiesArray = allCities.filter(city => city.posts != undefined);
-    setCities(citiesArray);
+    // const citiesRes = await fetch(`https://xplore-cf984-default-rtdb.europe-west1.firebasedatabase.app/cities.json`);
+    // const citiesData = await citiesRes.json();
+    // const allCities = Object.keys(citiesData).map(key => ({ id: key, ...citiesData[key]})); // from object to array
+    // // const citiesArray = allCities.filter(city => city.posts != undefined);
+
+    //  // check if cities have any posts
+    //  const result = allCities.map((city) => ({
+    //   data: city,
+    //   match: allPosts.some((post) => post.cityId === city.id)
+    //   })).filter(res => res.match == true);
+    //   const citiesWithPosts = result.map(res => res.data);
+    //   console.log(citiesWithPosts);
+  
+    //   if(result.length != 0) {
+    //     setCities(citiesWithPosts);
+    //   }
+
+
+    onValue(citiesRef, async snapshot => {
+      const citiesArray= [];
+      snapshot.forEach(postSnapshot => {
+        const id = postSnapshot.key;
+        const data = postSnapshot.val();
+        const city = {
+            id,
+            ...data
+        };
+        citiesArray.push(city);
+      });
+      setCities(citiesArray);
+    });
   }
 
   async function getUserName() {
@@ -61,7 +117,6 @@ export default function HomePage(){
   }
 
   useEffect(() => {
-
     setUser(activeUser);
 
     async function getUserDataFromDB() {
@@ -78,11 +133,10 @@ export default function HomePage(){
   }, [activeUser, user]);
 
   useIonViewWillEnter(() => {
-    
     getCountries();
     getCities();
     getUserName();
-  });
+  }, []);
 
   const slideOpts = {
     initialSlide: 0,
@@ -145,25 +199,14 @@ export default function HomePage(){
           
           <IonItem lines="none">
           <IonSlides options={slide2Opts}>
-            {countries.map((country, index) => {
-                return (
-                  <IonSlide className='ion-slide' key={`slide_${index}`}>
-                    <CountryItem country={country} key={country.id} /> 
-                  </IonSlide> 
-                )
-              })}
+            {countries.map((country, index) => 
+              <IonSlide className='ion-slide' key={`slide_${index}`}>
+                <CountryItem country={country} key={country.id} /> 
+              </IonSlide> 
+            )}
           </IonSlides>
-
-            {/* <IonSlides options={slide2Opts}>
-            {countries.map((country, index) => {
-              return (
-                <IonSlide className='ion-slide' key={`slide_${index}`}>
-                  <CountryItem country={country} key={country.id} />  
-                </IonSlide>
-              )
-            })}
-            </IonSlides> */}
           </IonItem>
+
         </IonList>
       </IonContent>
     </IonPage>
